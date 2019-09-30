323df4b9673e61e53d3301d3c13a73e52e9bb368
Serena Nguyen <serena.nguyen@mutualmobile.com>
2019-09-30 14:13:48 -0500

    remove nav js

--- scripts/main.js
+++ scripts/main.js
@@ -3,15 +3,3 @@ $("#home-button").click(function() {
         scrollTop: $("#landing").offset().top
     }, 1000);
 });
-
-$(".bio-button").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#bio").offset().top
-    }, 1000);
-});
-
-$(".projects-button").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#projects").offset().top
-    }, 1000);
-});
