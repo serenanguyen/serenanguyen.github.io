fa17bc1f52ae5bbe6cf79881b8491837e795497d
Serena Nguyen <serena.nguyen@mutualmobile.com>
2019-10-04 11:16:39 -0500

    spacing and link color change

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,10 @@
 $("#home-button").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#landing").offset().top
-    }, 1000);
+    // $([document.documentElement, document.body]).animate({
+    //     scrollTop: $("#landing").offset().top
+    // }, 1000);
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+        });
 });
